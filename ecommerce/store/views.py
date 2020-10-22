7011142cbe18dddfb5b54d8c4c4290668e6dc775
bishalpokharel325 <bishalpokharel325@gmail.com>
2020-10-22 22:04:06 +0545

    commit after defining templates, url and views

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'frontend/pages/home.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'frontend/pages/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'frontend/pages/checkout.html', context)
